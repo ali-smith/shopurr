a3fc6d36358abe986bc7067cab37819bc533af7d
Ali Smith <alipsmith@me.com>
2015-01-10 11:49:26 -0800

    ordered list.  bam.

--- js/app.js
+++ js/app.js
@@ -7,13 +7,32 @@ $(document).ready (function() {
 $("input[type='text']").on('keydown',function(e) {
   	if(e.keyCode==13) {
   		var newItem = $(this).val(); 
-  		 $('.listItems').prepend('<li class="li-box"><input type="checkbox"/>' + newItem + '</li>');
+  		 $('.listItems-unchecked').prepend('<li class="li-box-unchecked"><input type="checkbox"/>' + newItem + '</li>');
   }
   });
 
-$('.listItems').on('click', "input[type='checkbox']", function() {
-		$(this).parent().remove();
-});		
+
+$('.listItems-unchecked').on('change', "input[type='checkbox']", function() {
+		$(this).parent().toggleClass('li-box-checked','li-box-unchecked').prependTo('.listItems-checked');
+
+			
+})
+
+$('.listItems-checked').on('change', "input[type='checkbox']", function() {
+		$(this).parent().toggleClass('li-box-checked','li-box-unchecked').appendTo('.listItems-unchecked');
+
+});
+
+// --OR-- maybe it's just a matter of changing classes from unchecked to checked like in ana's fucking code:
+// $("li > i").first().on("click", function(){
+// 			if($(this).hasClass("fa-square-o")) {
+// 				$(this).parent().removeClass("active-item").addClass("completed-item");
+// 				$(this).removeClass("fa-square-o").addClass("fa-check-square-o");
+// 			} else {
+// 				$(this).parent().removeClass("completed-item").addClass("active-item");
+// 				$(this).removeClass("fa-check-square-o").addClass("fa-square-o");
+// 			}
+// 		});		
 	
 
 
