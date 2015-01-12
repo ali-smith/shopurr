2bf0064cc9a44bc5499181e1546c626631b5c4b9
Ali Smith <alipsmith@me.com>
2015-01-11 19:51:21 -0800

    added if/else to jquery

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
 $(document).ready (function() {
 ///
 
+// $("input[type='text']").on('keyup',function(e) {
+//   	if(e.keyCode==13) {
+//   		var newItem = $(this).val();
+//   	if (value>='1') {	 
+//   		 $('.listItems-unchecked').prepend('<li class="li-box-unchecked"><input type="checkbox"/>' + newItem + '</li>');
+//   		 $(this).val('');
+//   		}
 
+//   	else{ event.preventDefault();
+
+// 		} 	 	
+
+//   }		
+//   });
+
+//1/
 $("input[type='text']").on('keyup',function(e) {
   	if(e.keyCode==13) {
   		var newItem = $(this).val(); 
@@ -10,22 +25,26 @@ $("input[type='text']").on('keyup',function(e) {
 
   }		
   });
-///
 
+//2/
 $('.listItems-unchecked').on('change', "input[type='checkbox']", function() {
 		$(this).parent().toggleClass('li-box-checked','li-box-unchecked').prependTo('.listItems-checked');
 			
 });
-//3/
 
+//3/
 $('.listItems-checked').on('change', "input[type='checkbox']", function() {
 		$(this).parent().toggleClass('li-box-checked','li-box-unchecked').appendTo('.listItems-unchecked');
 });
 //4/
-		
-	
-
-
-
-
 });//document ready/
+
+
+// $("input[type='text']").on('keyup',function(e) {
+//   	if(e.keyCode==13) {
+//   		var newItem = $(this).val(); 
+//   		 $('.listItems-unchecked').prepend('<li class="li-box-unchecked"><input type="checkbox"/>' + newItem + '</li>');
+//   		 $(this).val('');	
+
+//   }		
+//   });
