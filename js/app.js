c0e0468b1d7d441ffc3337e9db5f2bd70bcfd00b
Ali Smith <alipsmith@me.com>
2015-01-16 18:52:10 -0800

    final?

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ $("input[name='addItem']").on('keyup',function(e) {
   if(e.keyCode==13) {
       var newItem = $.trim($(this).val());
       if (newItem.length > 0){
-       $('.listItems-unchecked').hide().prepend('<li class="li-box-unchecked"><img src="images/box.png" class="box">' + newItem + '<img src="images/ex.png" class="ex"></li>').fadeIn('normal');
+       $('.listItems-unchecked').hide().prepend('<li class="li-box-unchecked"><img src="images/box.png" id="box">' + newItem + '<img src="images/ex.png" id="ex"></li>').fadeIn('normal');
        $(this).val('');
       }
     else {
@@ -60,7 +60,7 @@ $('.listItems-checked').on('click', 'img', function() {
 });
 
 //4
-$(document).on('click', '.ex', function() {
+$(document).on('click', '#ex', function() {
 				$(this).closest('li').remove();
 			
 			
