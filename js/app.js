13e368a994b96d8963a9dbf3b3dcbc084591f814
Ali Smith <alipsmith@me.com>
2015-04-17 13:17:51 -0700

    all

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 $(document).ready (function() {
 
-/*adds new list name dynamically*/
-$("input[name='newList']").on('keyup',function(e) {
+//adds new list name dynamically
+$("input[id='newListInput']").on('keyup',function(e) {
 	if(e.keyCode==13) {
-	var newListh1 = $.trim($(this).val());
-	if (newListh1.length > 0){
-		$('.newListName').empty();
-		$('.listItems-unchecked').empty();
-    	$('.listItems-checked').empty();
-		$('.newListName').prepend(newListh1 + '<span class="scratchList"><p>scratch list</p></span');
-		$('.newListHidden').fadeIn('normal');
+	var listName = $.trim($(this).val());
+	if (listName.length > 0){
+		// $.trim($(this).val());
 		$(this).val('').blur();
+		$('#newListInput').hide();
+		$('.scratchList').fadeIn();
+		$('.listName').html(listName);
+		$('.listNameWrapper').fadeIn();
+		$('.listWrapper').fadeIn();
+		
 	}
 else {
       //do nothing
@@ -18,21 +20,13 @@ else {
   }
 });
 
-/*deletes new list including new list name*/
-$('.newListName').on('click', '.scratchList', function () {
-	$(this).remove();
-	$('.listItems-unchecked').empty();
-    $('.listItems-checked').empty();
-    $('.newListHidden').hide();
-
-});
-
-/*adds new list item (with one or more character) on 'enter'*/
-$("input[name='addItem']").on('keyup',function(e) {
+//adds new list item (with one or more character) on 'enter'
+$("input[id='newItem']").on('keyup',function(e) {
   if(e.keyCode==13) {
       var newItem = $.trim($(this).val());
       if (newItem.length > 0){
-       $('.listItems-unchecked').hide().prepend('<li class="li-box-unchecked"><span class="box"></span>' + newItem + '<span class="ex"></span></li>').fadeIn('normal');
+      $('.scratchItems').fadeIn();
+       $('.listItems').prepend('<li><i class="fa fa-square-o"></i><i class="hidden fa fa-check-square-o"></i><p>' + newItem + '</p><i class="fa fa-times"></i></li>').fadeIn('normal');
        $(this).val('');
       }
     else {
@@ -41,49 +35,48 @@ $("input[name='addItem']").on('keyup',function(e) {
   }
 });
 
-/*changes empty checkbox to checkmark and changes color*/
-$('.listItems-unchecked').on('click', '.box', function() {
-		$(this).parent().removeClass('li-box-unchecked').addClass('li-box-checked').hide().prependTo('.listItems-checked').fadeIn('normal');
-		$(this).removeClass('box').addClass('check');		 
+// checked items move to bottom of list
+$('.listItems').on('click', '.fa-square-o', function(event) {
+	event.preventDefault();
+	$(this).hide();
+	$(this).siblings('.fa-check-square-o').show();
+	$(this).parent().addClass('gray');
+	$(this).parent().hide().appendTo('.listItems').fadeIn('slow');
 });
 
-/*changes checkmark to checkbox and changes color back*/
-$('.listItems-checked').on('click', '.check', function() {
-		$(this).parent().removeClass('li-box-checked').addClass('li-box-unchecked').hide().appendTo('.listItems-unchecked').fadeIn('normal');
-		$(this).removeClass('check').addClass('box');
+//unchecked items move to top of list
+$('.listItems').on('click', '.fa-check-square-o', function(event) {
+	event.preventDefault();
+	$(this).hide();
+	$(this).siblings('.fa-square-o').show();
+	$(this).parent().removeClass('gray');
+	$(this).parent().hide().prependTo('.listItems').fadeIn('slow');
 });
 
-/*deletes item from list*/
-$(document).on('click', '.ex', function() {
-				$(this).closest('li').fadeOut('normal');			
+//removes item from list
+$('.listItems').on('click', '.fa-times', function(event) {
+	event.preventDefault();
+	$(this).parent().fadeOut();
 });
 
-/*deletes all items from list*/
-$(document).on('click', '.scratchItems', function(){
-    $('.listItems-unchecked').empty();
-    $('.listItems-checked').empty();
+//removes entire list
+$('.scratchList').on('click', function(event) {
+	event.preventDefault();
+	$(this).parent().fadeOut();
+	$('.listWrapper').fadeOut();
+	$('.listItems').empty();
+	$('#newListInput').delay(500).fadeIn();
 });
 
+//removes all list items
+$('.listWrapper').on('click', '.scratchItems', function(event) {
+	event.preventDefault();
+	$('.listItems').empty();
+	$(this).fadeOut(500);
+	$('#newItem').focus();
+});
 
-});//document ready/
+});//document ready
 
 
-// /*for future use*/
-// /*shows saved lists on hover*/
-// $('.listNames').on('mouseenter', function () {
-// 	$('.savedLists').fadeIn('normal');
-// });
-
-// /*hides saved lists on mouseleave*/
-// $('.listNames').on ('mouseleave', function() {
-// 					$('.savedLists').fadeOut('normal');
-// });
-
-// $('.newListName').on('mouseenter', function() {
-// 	$(this).children().show();
-// });
-
-// $('.newListName').on('mouseenter', function() {
-// 	$(this).children().show();
-// });
 
